8ed4b7e95e36ae946f55cde4a13eeec3023d7d5e
YuanHu <yuan.hu1@zte.com.cn>
2016-10-09 15:40:11 +0800

    Read plan input parameters from file.json. And save to the file named Definitions/plans.yaml.
    Change-Id: I19a34ee6cdcada2b3e58b18b4c611ed7ffd1c175
    Signed-off-by: YuanHu <yuan.hu1@zte.com.cn>

--- modeldesigner/winery.repository.ext/src/main/java/org/eclipse/winery/repository/ext/export/yaml/switcher/subswitcher/PlansXml2YamlSubSwitch.java
+++ modeldesigner/winery.repository.ext/src/main/java/org/eclipse/winery/repository/ext/export/yaml/switcher/subswitcher/PlansXml2YamlSubSwitch.java
@@ -15,16 +15,36 @@
  */
 package org.eclipse.winery.repository.ext.export.yaml.switcher.subswitcher;
 
-import org.eclipse.winery.model.tosca.TParameter;
+import org.eclipse.winery.common.RepositoryFileReference;
+import org.eclipse.winery.common.Util;
+import org.eclipse.winery.common.ids.XMLId;
+import org.eclipse.winery.common.ids.definitions.ServiceTemplateId;
+import org.eclipse.winery.common.ids.elements.PlanId;
+import org.eclipse.winery.common.ids.elements.PlansId;
 import org.eclipse.winery.model.tosca.TPlan;
 import org.eclipse.winery.model.tosca.TPlans;
 import org.eclipse.winery.model.tosca.TServiceTemplate;
+import org.eclipse.winery.repository.backend.Repository;
 import org.eclipse.winery.repository.ext.export.yaml.switcher.Xml2YamlSwitch;
 import org.eclipse.winery.repository.ext.yamlmodel.Input;
 import org.eclipse.winery.repository.ext.yamlmodel.Plan;
+import org.eclipse.winery.repository.resources.servicetemplates.ServiceTemplateResource;
+import org.eclipse.winery.repository.resources.servicetemplates.ServiceTemplatesResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
 /**
@@ -33,6 +53,8 @@ import java.util.Map.Entry;
  *
  */
 public class PlansXml2YamlSubSwitch extends AbstractXml2YamlSubSwitch {
+  private static final Logger logger = LoggerFactory.getLogger(PlansXml2YamlSubSwitch.class);
+
 
 	public PlansXml2YamlSubSwitch(Xml2YamlSwitch parentSwitch) {
 		super(parentSwitch);
@@ -75,19 +97,104 @@ public class PlansXml2YamlSubSwitch extends AbstractXml2YamlSubSwitch {
       // planLanguage
       yplan.setPlanLanguage(validataPlanLanguage(tPlan.getPlanLanguage()));
       // inputs
-      TPlan.InputParameters tinputs = tPlan.getInputParameters();
-      if (tinputs != null && tinputs.getInputParameter() != null && !tinputs.getInputParameter().isEmpty()) {
-        for (TParameter tinput: tinputs.getInputParameter()) {
-          Input yinput = new Input();
-          yinput.setType(Xml2YamlSwitchUtils.convert2YamlType(tinput.getType()));
-          yinput.setEntry_schema(Xml2YamlSwitchUtils.convert2YamlEntrySchema(tinput.getType()));
-          yinput.setRequired(Xml2YamlSwitchUtils.convert2YamlValue(tinput.getRequired()));
-          yplan.getInputs().put(tinput.getName(), yinput);
-        }
-      }
-
+      String s = readInputsInfoFromFile(tPlan);
+      logger.info("ReadInputsInfoFromFile s = " + s);
+      yplan.getInputs().putAll(parseInputs(s));
+//      TPlan.InputParameters tinputs = tPlan.getInputParameters();
+//      if (tinputs != null && tinputs.getInputParameter() != null && !tinputs.getInputParameter().isEmpty()) {
+//        for (TParameter tinput: tinputs.getInputParameter()) {
+//          Input yinput = new Input();
+//          yinput.setType(Xml2YamlSwitchUtils.convert2YamlType(tinput.getType()));
+//          yinput.setEntry_schema(Xml2YamlSwitchUtils.convert2YamlEntrySchema(tinput.getType()));
+//          yinput.setRequired(Xml2YamlSwitchUtils.convert2YamlValue(tinput.getRequired()));
+//          yplan.getInputs().put(tinput.getName(), yinput);
+//        }
+//      }
       
       return Xml2YamlSwitchUtils.buildEntry(tPlan.getName(), yplan);
+    }
+    
+    /**
+     * @param s
+     * @return
+     */
+    private Map<String, Input> parseInputs(String s) {
+      JsonArray jsonArray = new Gson().fromJson(s, JsonArray.class);
+      if (jsonArray != null) {
+        for (JsonElement jsonElement : jsonArray) {
+          if (jsonElement.isJsonObject()) {
+            JsonObject jsonObject = (JsonObject)jsonElement;
+            if (jsonObject.has("name") && jsonObject.has("output")) {
+              String name = jsonObject.get("name").getAsString();
+              if ("StartEvent".equalsIgnoreCase(name)) {
+                JsonElement jInputs = jsonObject.get("output");
+                if (jInputs.isJsonObject()) {
+                  return parseInputs((JsonObject)jInputs);
+                }
+              }
+            }
+          }
+        }
+      }
+      
+      return new HashMap<>();
+    }
+
+    /**
+     * @param jInputs
+     * @return
+     */
+    private Map<String, Input> parseInputs(JsonObject jInputs) {
+      Map<String, Input> inputs = new HashMap<>();
+      Iterator<Entry<String, JsonElement>> iterator = jInputs.entrySet().iterator();
+      while (iterator.hasNext()) {
+        Entry<String, JsonElement> next = iterator.next();
+        String name = next.getKey();
+        JsonObject value = (JsonObject) next.getValue();
+        
+        Input input = new Input();
+        input.setType(value.get("type").getAsString());
+        input.setDefault(value.get("value").getAsString());
+        
+        inputs.put(name, input);
+      }
+      
+      return inputs;
+    }
+
+    private final String INPUTS_INFO_FILE_NAME = "file.json";
+
+    private String readInputsInfoFromFile(TPlan tPlan) {
+      TServiceTemplate tst = getTServiceTemplate();
+      ServiceTemplateResource str = new ServiceTemplatesResource().
+          getComponentInstaceResource(Util.URLencode(tst.getTargetNamespace()), tst.getId());
+      PlansId plansId = new PlansId((ServiceTemplateId) str.getId());
+      PlanId planId = new PlanId(plansId, new XMLId(tPlan.getId(), false));
+      
+      InputStream ins = null;
+      try {
+        RepositoryFileReference rfr = new RepositoryFileReference(planId, INPUTS_INFO_FILE_NAME);
+        ins = Repository.INSTANCE.newInputStream(rfr);
+        byte b[] = new byte[1024*1024];   
+        int len = 0;
+        int temp = 0;
+        while((temp = ins.read()) != -1){
+            b[len]=(byte)temp;
+            len ++;
+        }
+        return new String(b, 0, len); 
+      } catch (IOException e) {
+        logger.warn("Read inputs from file.json failed. Plan name = " + tPlan.getName(), e);
+      } finally {
+        if (ins != null) {
+          try {
+            ins.close();
+          } catch (IOException e) {
+          }
+        }
+      }
+      
+      return "";
     }
 
     private String buildReference(TPlan tPlan) {
@@ -95,8 +202,8 @@ public class PlansXml2YamlSubSwitch extends AbstractXml2YamlSubSwitch {
       if (tReference == null) {
         return "";
       }
-      String[] tmps = tReference.split("[\\/]");
-      return "plan" + File.separator + tmps[tmps.length-1];
+
+      return "plan" + File.separator + new File(tReference).getName();
     }
 
     /**
