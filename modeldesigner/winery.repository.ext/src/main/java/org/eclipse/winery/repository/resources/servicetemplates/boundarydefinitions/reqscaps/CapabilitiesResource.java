cc5a1ab99e0ab50e30577aefe70201d8a59d9729
qinlihan <qin.lihan@zte.com.cn>
2016-10-28 15:20:47 +0800

    substitutable nodetype is lack of Req/Caps which are add in substitutable servicetemplate
    Change-Id: Iff9fb8e01dc5c95e8d17e95081d59a60d4e1dad4
    Issue-id: TOSCA-156
    Signed-off-by: qinlihan <qin.lihan@zte.com.cn>

--- modeldesigner/winery.repository.ext/src/main/java/org/eclipse/winery/repository/resources/servicetemplates/boundarydefinitions/reqscaps/CapabilitiesResource.java
+++ modeldesigner/winery.repository.ext/src/main/java/org/eclipse/winery/repository/resources/servicetemplates/boundarydefinitions/reqscaps/CapabilitiesResource.java
@@ -30,6 +30,8 @@ import javax.ws.rs.core.Response.Status;
 import javax.xml.namespace.QName;
 
 import org.eclipse.winery.common.ModelUtilities;
+import org.eclipse.winery.common.servicetemplate.boundarydefinitions.reqscaps.ReqCapInfo;
+import org.eclipse.winery.common.servicetemplate.boundarydefinitions.reqscaps.ReqsCapsRequest;
 import org.eclipse.winery.model.tosca.TCapability;
 import org.eclipse.winery.model.tosca.TCapabilityRef;
 import org.eclipse.winery.repository.backend.BackendUtils;
@@ -44,104 +46,106 @@ import com.sun.jersey.api.view.Viewable;
  * This class is an adaption from
  * {@link org.eclipse.winery.repository.resources.servicetemplates.boundarydefinitions.reqscaps.RequirementsResource}
  */
-public class CapabilitiesResource
-        extends EntityWithoutIdCollectionResource<CapabilityResource, TCapabilityRef> {
+public class CapabilitiesResource extends
+    EntityWithoutIdCollectionResource<CapabilityResource, TCapabilityRef> {
 
-    public CapabilitiesResource(IPersistable res, List<TCapabilityRef> refs) {
-        super(CapabilityResource.class, TCapabilityRef.class, refs, res);
+  public CapabilitiesResource(IPersistable res, List<TCapabilityRef> refs) {
+    super(CapabilityResource.class, TCapabilityRef.class, refs, res);
+  }
+
+  @Override
+  public Viewable getHTML() {
+    throw new IllegalStateException(
+        "Not yet required: boundarydefinitions.jsp renders all tab content.");
+  }
+
+  @POST
+  @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+  public Response addNewElement(@FormParam("name") String name, @FormParam("ref") String reference) {
+    // Implementation adapted from super addNewElement
+
+    if (reference == null) {
+      return Response.status(Status.BAD_REQUEST).entity("A reference has to be provided").build();
     }
 
-    @Override
-    public Viewable getHTML() {
-        throw new IllegalStateException(
-                "Not yet required: boundarydefinitions.jsp renders all tab content.");
+    TCapabilityRef ref = new TCapabilityRef();
+    ref.setName(name); // may also be null
+
+    // The XML model fordces us to put a reference to the object and not just the string
+    ServiceTemplateResource rs = (ServiceTemplateResource) this.res;
+    TCapability resolved = ModelUtilities.resolveCapability(rs.getServiceTemplate(), reference);
+    // In case nothing was found: report back to the user
+    if (resolved == null) {
+      return Response.status(Status.BAD_REQUEST).entity("Reference could not be resolved").build();
     }
 
-    @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public Response addNewElement(@FormParam("name") String name, @FormParam("ref") String reference) {
-        // Implementation adapted from super addNewElement
+    ref.setRef(resolved);
 
-        if (reference == null) {
-            return Response.status(Status.BAD_REQUEST).entity("A reference has to be provided")
-                    .build();
-        }
+    // "this.alreadyContains(ref)" cannot be called as this leads to a mappable exception: The
+    // data does not contain an id where the given ref attribute may point to
 
-        TCapabilityRef ref = new TCapabilityRef();
-        ref.setName(name); // may also be null
+    this.list.add(ref);
+    return CollectionsHelper.persist(this.res, this, ref);
+  }
 
-        // The XML model fordces us to put a reference to the object and not just the string
-        ServiceTemplateResource rs = (ServiceTemplateResource) this.res;
-        TCapability resolved = ModelUtilities.resolveCapability(rs.getServiceTemplate(), reference);
-        // In case nothing was found: report back to the user
-        if (resolved == null) {
-            return Response.status(Status.BAD_REQUEST).entity("Reference could not be resolved")
-                    .build();
-        }
+  @POST
+  @Path("list/")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Response addElements(ReqsCapsRequest request) {
 
-        ref.setRef(resolved);
+    addCapabilities(request);
 
-        // "this.alreadyContains(ref)" cannot be called as this leads to a mappable exception: The
-        // data does not contain an id where the given ref attribute may point to
+    return BackendUtils.persist(this.res);
+  }
 
-        this.list.add(ref);
-        return CollectionsHelper.persist(this.res, this, ref);
+  public void addCapabilities(ReqsCapsRequest request) {
+    if (null == request || null == request.getReqcapList()) {
+      return;
     }
 
-    @POST
-    @Path("list/")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response addElements(ReqsCapsRequest request) {
+    this.list.clear();
+    for (ReqCapInfo info : request.getReqcapList()) {
+      if (null == info.getName() || null == info.getRef()) {
+        continue;
+      }
 
-        if (null == request || null == request.getReqcapList()) {
-            return Response.status(Status.BAD_REQUEST).entity("request is empty!").build();
-        }
+      ServiceTemplateResource rs = (ServiceTemplateResource) this.res;
+      TCapability resolved =
+          ModelUtilities.resolveCapability(rs.getServiceTemplate(), info.getRef());
+      if (resolved == null) {
+        continue;
+      }
 
-        this.list.clear();
-        for (ReqCapInfo info : request.getReqcapList()) {
-            if (null == info.getName() || null == info.getRef()) {
-                continue;
-            }
+      TCapabilityRef ref = new TCapabilityRef();
+      ref.setName(info.getName());
+      ref.setRef(resolved);
 
-            ServiceTemplateResource rs = (ServiceTemplateResource) this.res;
-            TCapability resolved =
-                    ModelUtilities.resolveCapability(rs.getServiceTemplate(), info.getRef());
-            if (resolved == null) {
-                continue;
-            }
-
-            TCapabilityRef ref = new TCapabilityRef();
-            ref.setName(info.getName());
-            ref.setRef(resolved);
-
-            this.list.add(ref);
-        }
-
-        return BackendUtils.persist(this.res);
+      this.list.add(ref);
     }
+  }
 
-    @GET
-    @Path("list/")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getElements() {
-        List<ReqCapInfo> reqcapList = new ArrayList<ReqCapInfo>();
-        if (this.list != null) {
-            for (TCapabilityRef ref : this.list) {
-                ReqCapInfo info = buildReqCapInfo(ref);
-                reqcapList.add(info);
-            }
-        }
-        return Response.ok().entity(reqcapList).build();
+  @GET
+  @Path("list/")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getElements() {
+    List<ReqCapInfo> reqcapList = new ArrayList<ReqCapInfo>();
+    if (this.list != null) {
+      for (TCapabilityRef ref : this.list) {
+        ReqCapInfo info = buildReqCapInfo(ref);
+        reqcapList.add(info);
+      }
     }
+    return Response.ok().entity(reqcapList).build();
+  }
 
-    private ReqCapInfo buildReqCapInfo(TCapabilityRef ref) {
-        ReqCapInfo info = new ReqCapInfo();
-        info.setName(ref.getName());
-        TCapability cap = (TCapability) ref.getRef();
-        info.setRef(cap.getId());
-        String refNodeName = cap.getOtherAttributes().get(new QName("refNodeName"));
-        info.setNodeName(refNodeName);
-        info.setRefName(cap.getName());
-        return info;
-    }
+  private ReqCapInfo buildReqCapInfo(TCapabilityRef ref) {
+    ReqCapInfo info = new ReqCapInfo();
+    info.setName(ref.getName());
+    TCapability cap = (TCapability) ref.getRef();
+    info.setRef(cap.getId());
+    String refNodeName = cap.getOtherAttributes().get(new QName("refNodeName"));
+    info.setNodeName(refNodeName);
+    info.setRefName(cap.getName());
+    return info;
+  }
 }
--- modeldesigner/winery.repository.ext/src/main/java/org/eclipse/winery/repository/resources/servicetemplates/boundarydefinitions/reqscaps/RequirementsResource.java
+++ modeldesigner/winery.repository.ext/src/main/java/org/eclipse/winery/repository/resources/servicetemplates/boundarydefinitions/reqscaps/RequirementsResource.java
@@ -30,8 +30,8 @@ import javax.ws.rs.core.Response.Status;
 import javax.xml.namespace.QName;
 
 import org.eclipse.winery.common.ModelUtilities;
-import org.eclipse.winery.model.tosca.TCapability;
-import org.eclipse.winery.model.tosca.TCapabilityRef;
+import org.eclipse.winery.common.servicetemplate.boundarydefinitions.reqscaps.ReqCapInfo;
+import org.eclipse.winery.common.servicetemplate.boundarydefinitions.reqscaps.ReqsCapsRequest;
 import org.eclipse.winery.model.tosca.TRequirement;
 import org.eclipse.winery.model.tosca.TRequirementRef;
 import org.eclipse.winery.repository.backend.BackendUtils;
@@ -46,114 +46,115 @@ import com.sun.jersey.api.view.Viewable;
  * This class is mirrored at
  * {@link org.eclipse.winery.repository.resources.servicetemplates.boundarydefinitions.reqscaps.CapabilitiesResource}
  */
-public class RequirementsResource
-        extends EntityWithoutIdCollectionResource<RequirementResource, TRequirementRef> {
+public class RequirementsResource extends
+    EntityWithoutIdCollectionResource<RequirementResource, TRequirementRef> {
 
-    public RequirementsResource(IPersistable res, List<TRequirementRef> refs) {
-        super(RequirementResource.class, TRequirementRef.class, refs, res);
+  public RequirementsResource(IPersistable res, List<TRequirementRef> refs) {
+    super(RequirementResource.class, TRequirementRef.class, refs, res);
+  }
+
+  @Override
+  public Viewable getHTML() {
+    throw new IllegalStateException(
+        "Not yet required: boundarydefinitions.jsp renders all tab content.");
+  }
+
+  /**
+   * Adds an element using form-encoding
+   * 
+   * This is necessary as TRequirementRef contains an IDREF and the XML snippet itself does not
+   * contain the target id
+   * 
+   * @param name the optional name of the requirement
+   * @param reference the reference to a requirement in the topology
+   */
+  @POST
+  @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+  public Response addNewElement(@FormParam("name") String name, @FormParam("ref") String reference) {
+    // Implementation adapted from super addNewElement
+
+    if (reference == null) {
+      return Response.status(Status.BAD_REQUEST).entity("A reference has to be provided").build();
     }
 
-    @Override
-    public Viewable getHTML() {
-        throw new IllegalStateException(
-                "Not yet required: boundarydefinitions.jsp renders all tab content.");
+    TRequirementRef ref = new TRequirementRef();
+    ref.setName(name); // may also be null
+
+    // The XML model forces us to put a reference to the object and not just the string
+    ServiceTemplateResource rs = (ServiceTemplateResource) this.res;
+    TRequirement resolved = ModelUtilities.resolveRequirement(rs.getServiceTemplate(), reference);
+    // In case nothing was found: report back to the user
+    if (resolved == null) {
+      return Response.status(Status.BAD_REQUEST).entity("Reference could not be resolved").build();
     }
 
-    /**
-     * Adds an element using form-encoding
-     * 
-     * This is necessary as TRequirementRef contains an IDREF and the XML snippet itself does not
-     * contain the target id
-     * 
-     * @param name the optional name of the requirement
-     * @param reference the reference to a requirement in the topology
-     */
-    @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public Response addNewElement(@FormParam("name") String name, @FormParam("ref") String reference) {
-        // Implementation adapted from super addNewElement
+    ref.setRef(resolved);
 
-        if (reference == null) {
-            return Response.status(Status.BAD_REQUEST).entity("A reference has to be provided")
-                    .build();
-        }
+    // "this.alreadyContains(ref)" cannot be called as this leads to a mappable exception: The
+    // data does not contain an id where the given ref attribute may point to
 
-        TRequirementRef ref = new TRequirementRef();
-        ref.setName(name); // may also be null
+    this.list.add(ref);
+    return CollectionsHelper.persist(this.res, this, ref);
+  }
 
-        // The XML model forces us to put a reference to the object and not just the string
-        ServiceTemplateResource rs = (ServiceTemplateResource) this.res;
-        TRequirement resolved =
-                ModelUtilities.resolveRequirement(rs.getServiceTemplate(), reference);
-        // In case nothing was found: report back to the user
-        if (resolved == null) {
-            return Response.status(Status.BAD_REQUEST).entity("Reference could not be resolved")
-                    .build();
-        }
+  @POST
+  @Path("list/")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Response addElements(ReqsCapsRequest request) {
 
-        ref.setRef(resolved);
+    addRequirements(request);
 
-        // "this.alreadyContains(ref)" cannot be called as this leads to a mappable exception: The
-        // data does not contain an id where the given ref attribute may point to
+    return BackendUtils.persist(this.res);
+  }
 
-        this.list.add(ref);
-        return CollectionsHelper.persist(this.res, this, ref);
+  public void addRequirements(ReqsCapsRequest request) {
+    if (null == request || null == request.getReqcapList()) {
+      return;
     }
 
-    @POST
-    @Path("list/")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response addElements(ReqsCapsRequest request) {
+    this.list.clear();
+    for (ReqCapInfo info : request.getReqcapList()) {
+      if (null == info.getName() || null == info.getRef()) {
+        continue;
+      }
 
-        if (null == request || null == request.getReqcapList()) {
-            return Response.status(Status.BAD_REQUEST).entity("request is empty!").build();
-        }
+      ServiceTemplateResource rs = (ServiceTemplateResource) this.res;
+      TRequirement resolved =
+          ModelUtilities.resolveRequirement(rs.getServiceTemplate(), info.getRef());
+      if (resolved == null) {
+        continue;
+      }
 
-        this.list.clear();
-        for (ReqCapInfo info : request.getReqcapList()) {
-            if (null == info.getName() || null == info.getRef()) {
-                continue;
-            }
+      TRequirementRef ref = new TRequirementRef();
+      ref.setName(info.getName());
+      ref.setRef(resolved);
 
-            ServiceTemplateResource rs = (ServiceTemplateResource) this.res;
-            TRequirement resolved =
-                    ModelUtilities.resolveRequirement(rs.getServiceTemplate(), info.getRef());
-            if (resolved == null) {
-                continue;
-            }
-
-            TRequirementRef ref = new TRequirementRef();
-            ref.setName(info.getName());
-            ref.setRef(resolved);
-
-            this.list.add(ref);
-        }
-
-        return BackendUtils.persist(this.res);
+      this.list.add(ref);
     }
+  }
 
-    @GET
-    @Path("list/")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getElements() {
-        List<ReqCapInfo> reqcapList = new ArrayList<ReqCapInfo>();
-        if (this.list != null) {
-            for (TRequirementRef ref : this.list) {
-                ReqCapInfo info = buildReqCapInfo(ref);
-                reqcapList.add(info);
-            }
-        }
-        return Response.ok().entity(reqcapList).build();
+  @GET
+  @Path("list/")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getElements() {
+    List<ReqCapInfo> reqcapList = new ArrayList<ReqCapInfo>();
+    if (this.list != null) {
+      for (TRequirementRef ref : this.list) {
+        ReqCapInfo info = buildReqCapInfo(ref);
+        reqcapList.add(info);
+      }
     }
+    return Response.ok().entity(reqcapList).build();
+  }
 
-    private ReqCapInfo buildReqCapInfo(TRequirementRef ref) {
-        ReqCapInfo info = new ReqCapInfo();
-        info.setName(ref.getName());
-        TRequirement req = (TRequirement) ref.getRef();
-        info.setRef(req.getId());
-        String refNodeName = req.getOtherAttributes().get(new QName("refNodeName"));
-        info.setNodeName(refNodeName);
-        info.setRefName(req.getName());
-        return info;
-    }
+  private ReqCapInfo buildReqCapInfo(TRequirementRef ref) {
+    ReqCapInfo info = new ReqCapInfo();
+    info.setName(ref.getName());
+    TRequirement req = (TRequirement) ref.getRef();
+    info.setRef(req.getId());
+    String refNodeName = req.getOtherAttributes().get(new QName("refNodeName"));
+    info.setNodeName(refNodeName);
+    info.setRefName(req.getName());
+    return info;
+  }
 }
