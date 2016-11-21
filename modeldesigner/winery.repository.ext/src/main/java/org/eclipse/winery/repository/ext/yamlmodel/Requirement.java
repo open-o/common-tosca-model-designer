cba09fbbbbcb75f5dbb52d5f16523b6c9ab72399
YuanHu <yuan.hu1@zte.com.cn>
2016-11-21 11:18:50 +0800

    Export 'relationship' of 'requirement assignment'.
    Change-Id: Ib2bf41664d7a03f2b5007e32839171a4b5f98773
    Issue-id: TOSCA-179
    Signed-off-by: YuanHu <yuan.hu1@zte.com.cn>

--- modeldesigner/winery.repository.ext/src/main/java/org/eclipse/winery/repository/ext/yamlmodel/Requirement.java
+++ modeldesigner/winery.repository.ext/src/main/java/org/eclipse/winery/repository/ext/yamlmodel/Requirement.java
@@ -17,77 +17,72 @@ package org.eclipse.winery.repository.ext.yamlmodel;
 
 
 public class Requirement {
-    private String node = "";
-    private String relationship = "";
-    private String capability = "";
-    private NodeFilter node_filter;
-    private int[] occurrences;
+  private String node = "";
+  private RequirementRelationship relationship;
+  private String capability = "";
+  private NodeFilter node_filter;
+  private int[] occurrences;
 
-    
-    public Requirement() {
-      super();
-    }
+  public Requirement() {
+    super();
+  }
 
-    public Requirement(NodeFilter node_filter) {
-      super();
-      this.node_filter = node_filter;
-    }
+  public Requirement(NodeFilter node_filter) {
+    super();
+    this.node_filter = node_filter;
+  }
 
-    public Requirement(String node) {
-      super();
+  public Requirement(String node) {
+    super();
+    this.node = node;
+  }
+
+  public String getNode() {
+    return node;
+  }
+
+  public void setNode(String node) {
+    if (node != null) {
       this.node = node;
     }
+  }
 
-    public String getNode() {
-        return node;
+  public RequirementRelationship getRelationship() {
+    return relationship;
+  }
+
+  public void setRelationship(RequirementRelationship relationship) {
+    this.relationship = relationship;
+  }
+
+  public String getCapability() {
+    return capability;
+  }
+
+  public void setCapability(String capability) {
+    if (capability != null) {
+      this.capability = capability;
     }
+  }
 
-    public void setNode(String node) {
-        if (node != null) {
-            this.node = node;
-        }
+  public NodeFilter getNode_filter() {
+    return node_filter;
+  }
+
+  public void setNode_filter(NodeFilter node_filter) {
+    if (node_filter != null) {
+      this.node_filter = node_filter;
     }
+  }
 
-    public String getRelationship() {
-        return relationship;
+  public int[] getOccurrences() {
+    return occurrences;
+  }
+
+  public void setOccurrences(int[] occurrences) {
+    if (occurrences != null && occurrences.length == 2) {
+      this.occurrences = occurrences;
     }
-
-    public void setRelationship(String relationship) {
-        if (relationship != null) {
-            this.relationship = relationship;
-        }
-    }
-
-    public String getCapability() {
-        return capability;
-    }
-
-    public void setCapability(String capability) {
-        if (capability != null) {
-            this.capability = capability;
-        }
-    }
-
-    public NodeFilter getNode_filter() {
-        return node_filter;
-    }
-
-    public void setNode_filter(NodeFilter node_filter) {
-        if (node_filter != null) {
-            this.node_filter = node_filter;
-        }
-    }
-
-    public int[] getOccurrences() {
-        return occurrences;
-    }
-
-    public void setOccurrences(int[] occurrences) {
-        if (occurrences != null && occurrences.length == 2) {
-            this.occurrences = occurrences;
-        }
-    }
-
-
+  }
 
 }
